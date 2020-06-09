a94f0eefd71b761bc97f83485161f218d41c6770
Dushyanth <kd13@iitbbs.ac.in>
2020-06-09 17:50:11 +0530

    Basic Layout and Styles

---
+++ align.js
@@ -0,0 +1,23 @@
+var unit=document.getElementsByClassName("unit");
+for( i=0;i<unit.length;i++){
+    unit[i].addEventListener("click",function(){
+    var topic = this.nextElementSibling;
+      if (topic.style.display ==="block") {
+          topic.style.display = "none";
+      } else {
+      topic.style.display = "block";
+    }
+    });
+}
+var scroller = document.getElementById("scrollUp");
+window.onscroll = function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    scroller.style.display = "block";
+  } else {
+    scroller.style.display = "none";
+  }
+}
+scroller.addEventListener("click",function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
