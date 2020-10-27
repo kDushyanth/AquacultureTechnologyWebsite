4e363cd6a55320d03edfd14061ed9910f86f1601
TVSSSRIPAD <tss11@iitbbs.ac.in>
2020-10-27 19:54:04 +0530

    added Basic Backend files

---
+++ models/Model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const MaterialSchema = new Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    topic : {
+        type:String,
+        required : [true , 'Please add correct topic name']   
+    },
+    urls : {
+        type: [String],
+    },
+});
+
+module.exports = mongoose.model('Materials' , MaterialSchema);
