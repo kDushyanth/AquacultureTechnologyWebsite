e71ae46497421e6380c93736efc7e3cab28b370c
TVSSSRIPAD <tss11@iitbbs.ac.in>
2020-10-27 19:59:16 +0530

    serving files from backend!

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const path = require('path')
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static('client'));
+app.get('/' , (req,res) => {
+    res.sendFile('./client/index.html', { root: __dirname });
+});
 
 const PORT = process.env.PORT || 5000 ; 
 app.listen(PORT , console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
