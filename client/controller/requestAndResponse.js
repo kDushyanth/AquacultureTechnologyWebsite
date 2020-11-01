a3cc51edefd3a01fc767c607cdb27dbe40085c02
TVSSSRIPAD <tss11@iitbbs.ac.in>
2020-11-01 12:41:30 +0530

    added topics.html page

--- client/controller/requestAndResponse.js
+++ client/controller/requestAndResponse.js
@@ -12,7 +12,22 @@ $(document).ready(function () {
 });
 
 
+
+const addTopicsAndMaterials = async (res) => {
+    var n = res.length;
+    
+    window.location.href = './topics.html'
+    
+    console.log(n);
+    
+    for(var i=0;i<n;i++){
+        console.log('in the loop');
+        $(".papers").append(`<a href="#" class="list-group-item list-group-item-action topic"> ${res[i].topic}</a>`);  
+    }
+}
+
 const getMaterialsController = async (semester,name) => {
     const response = await getMaterials(semester, name);
     console.log(response);
+    addTopicsAndMaterials(response.data);
 }
