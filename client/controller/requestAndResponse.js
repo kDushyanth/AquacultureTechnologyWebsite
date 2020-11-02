275c96fde62ec791e0cb56129c1a6c7112c54c0d
Dushyanth <kd13@iitbbs.ac.in>
2020-11-02 22:19:50 +0530

    created topic.html

--- client/controller/requestAndResponse.js
+++ client/controller/requestAndResponse.js
@@ -5,8 +5,9 @@ $(document).ready(function () {
             console.log();
             var name = event.target.innerHTML;
             var semester =  $(  $(  $(event.target).parent()[0]  ).parent()[0]).find("h5")[0].innerHTML ;
-            // console.log(name + ' is name ');
-            getMaterialsController( semester,name);
+            window.location.href='/topics.html?semester='+semester+'&chapter='+name;
+            
+        
         }
     });
     $("#submit").click((event)=>{
@@ -27,22 +28,3 @@ async function mailer(mail_content)  {
   console.log(res);
 }
 
-
-const addTopicsAndMaterials = async (res) => {
-    var n = res.length;
-    
-    window.location.href = './topics.html'
-    
-    console.log(n);
-    
-    for(var i=0;i<n;i++){
-        console.log('in the loop');
-        $(".papers").append(`<a href="#" class="list-group-item list-group-item-action topic"> ${res[i].topic}</a>`);  
-    }
-}
-
-const getMaterialsController = async (semester,name) => {
-    const response = await getMaterials(semester, name);
-    console.log(response);
-    addTopicsAndMaterials(response.data);
-}
---
+++ client/topic_hand.js
@@ -0,0 +1,53 @@
+$(document).ready(()=>{
+    console.log(window.location.search);
+    function getQueryParams() {
+        let result = {};
+        let params = window.location.search;
+        params = params.substr(1);
+        let queryParamArray = params.split('&');
+        queryParamArray.forEach(function(queryParam) {
+          let item = queryParam.split("=");
+          result[item[0]] = decodeURIComponent(item[1]);
+        });
+        return result;
+      }
+      const result = getQueryParams();
+      const info = get_info(result.chapter,result.semester);
+     
+});
+const get_info = async function(chapter,semester){
+    const info = await getMaterials(semester,chapter);
+     $('container > p').html(`${semester} | ${chapter}`);
+     for(var i=0;i<info.data.length;i++){
+       
+        $(".accordion").append(`
+        <div class="card">
+        <div class="card-header" id="heading${i}">
+          <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
+              ${info.data[i].topic}
+            </button>
+          </h2>
+        </div>
+    
+        <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordionExample">
+          <div class="card-body">
+             File
+          </div>
+          <hr>
+          <div class="card-body">
+             File
+          </div>
+          <hr>
+          <div class="card-body">
+             File
+          </div>
+          <hr>
+          <div class="card-body">
+             File
+          </div>
+        </div>
+        </div>
+        `);
+     }
+}
